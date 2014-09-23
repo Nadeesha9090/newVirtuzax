75b56cb15dd7e8f14676aa98b7e8fb7819ed4702
Nadeesha9090 <nadeesha9090@gmail.com>
2014-09-23 12:01:55 +0530

    Edited

---
+++ js/settings.js
@@ -0,0 +1,33 @@
+/*
+    jQuery Mobile Portfolio
+    Author: Sam Deering (samdeering.com)
+    Article: jquery4u.com/mobile/jquery-mobile-portfolio/
+    Demo: jquery4u.com/demos/mobileportfoliosite/
+*/
+
+(function($,W,D,undefined)
+{
+    //MOBILE SITE SETTINGS
+    W.JQMP = W.JQMP || {};
+    W.JQMP.SETTINGS =
+    {
+        //CONTACT FORM SETTINGS - enter your name and email where you would like your messages to be sent to
+        CONTACT:
+        {
+            name: 'Nadeesha',
+            email: 'nadeesha9090@gmail.com'
+        },
+
+        //SOCIAL NETWORK LINKS SETTINGS
+        SOCIAL:
+        {
+            
+            'twitter': 'http://twitter.com/jquery4u',
+            'facebook': 'https://www.facebook.com/Thaalakaz?ref=bookmarks',
+            'googleplus': 'https://plus.google.com/+NadeeshaThilakarathne9090/posts',
+            'linkedin': 'https://www.linkedin.com/profile/view?id=214719896&trk=nav_responsive_tab_profile',
+            
+        }
+    }
+
+})(jQuery,window,document);
